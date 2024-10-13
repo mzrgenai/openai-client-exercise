01f9bd6bfa1b385b250121e12c6b1c946cc02280
mzrgenai <mzrgenai@gmail.com>
2024-10-13 08:46:36 +0600

    Transcribe - audio to text in the same language

--- index.php
+++ index.php
@@ -11,10 +11,36 @@ $client = OpenAI::client($apiKey);
 
 use OpenAI;
 
-$response = $client->audio()->speech([
-    'model' => 'tts-1',
-    'input' => 'The quick brown fox jumped over the lazy dog.',
-    'voice' => 'alloy',
-]); // audio file content as string
+$response = $client->audio()->transcribe([
+    'model' => 'whisper-1',
+    'file' => fopen('audio-example.mp3', 'r'),
+    'response_format' => 'verbose_json',
+    'timestamp_granularities' => ['segment', 'word']
+]);
 
-file_put_contents('audio-example.mp3', $response);
+$response->task; // 'transcribe'
+$response->language; // 'english'
+$response->duration; // 2.95
+$response->text; // 'Hello, how are you?'
+
+foreach ($response->segments as $segment) {
+    $segment->index; // 0
+    $segment->seek; // 0
+    $segment->start; // 0.0
+    $segment->end; // 4.0
+    $segment->text; // 'Hello, how are you?'
+    $segment->tokens; // [50364, 2425, 11, 577, 366, 291, 30, 50564]
+    $segment->temperature; // 0.0
+    $segment->avgLogprob; // -0.45045216878255206
+    $segment->compressionRatio; // 0.7037037037037037
+    $segment->noSpeechProb; // 0.1076972484588623
+    $segment->transient; // false
+}
+
+foreach ($response->words as $word) {
+    $word->word; // 'Hello'
+    $word->start; // 0.31
+    $word->end; // 0.92
+}
+
+print_r($response->toArray()); // ['task' => 'transcribe', ...]
