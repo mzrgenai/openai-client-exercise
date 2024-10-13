7d794a48642d9605b04d9c9584762cf75a100d9c
mzrgenai <mzrgenai@gmail.com>
2024-10-13 08:43:21 +0600

    speech to audio

--- index.php
+++ index.php
@@ -11,47 +11,10 @@ $client = OpenAI::client($apiKey);
 
 use OpenAI;
 
-$response = $client->chat()->create([
-    'model' => 'gpt-3.5-turbo-0613',
-    'messages' => [
-        ['role' => 'user', 'content' => 'What\'s the weather like in Boston?'],
-    ],
-    'functions' => [
-        [
-            'name' => 'get_current_weather',
-            'description' => 'Get the current weather in a given location',
-            'parameters' => [
-                'type' => 'object',
-                'properties' => [
-                    'location' => [
-                        'type' => 'string',
-                        'description' => 'The city and state, e.g. San Francisco, CA',
-                    ],
-                    'unit' => [
-                        'type' => 'string',
-                        'enum' => ['celsius', 'fahrenheit']
-                    ],
-                ],
-                'required' => ['location'],
-            ],
-        ]
-    ]
-]);
+$response = $client->audio()->speech([
+    'model' => 'tts-1',
+    'input' => 'The quick brown fox jumped over the lazy dog.',
+    'voice' => 'alloy',
+]); // audio file content as string
 
-$response->id; // 'chatcmpl-6pMyfj1HF4QXnfvjtfzvufZSQq6Eq'
-$response->object; // 'chat.completion'
-$response->created; // 1677701073
-$response->model; // 'gpt-3.5-turbo-0613'
-
-foreach ($response->choices as $result) {
-    $result->index; // 0
-    $result->message->role; // 'assistant'
-    $result->message->content; // null
-    $result->message->functionCall->name; // 'get_current_weather'
-    $result->message->functionCall->arguments; // "{\n  \"location\": \"Boston, MA\"\n}"
-    $result->finishReason; // 'function_call'
-}
-
-$response->usage->promptTokens; // 82,
-$response->usage->completionTokens; // 18,
-$response->usage->totalTokens; // 100
+file_put_contents('audio-example.mp3', $response);
